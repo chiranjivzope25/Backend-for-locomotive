a038a9c1a87ae0a69b8625672840ef70ad36c865
Chiranjiv Zope <zchiranjiv@gmail.com>
2026-08-14 17:29:19 +0530

    adding new

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def predict(request: DualModelRequest):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
             detail=f"Inference Error: {str(e)}"
-        )s
+        )
